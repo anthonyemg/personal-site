b55433797e826032e727ac685e4112f8c9bb78db
Anthony Greenheck <aemgreenheck@gmail.com>
2017-07-27 13:08:56 -0400

    [added] links to linkedin and github

--- src/public/app/Me.jsx
+++ src/public/app/Me.jsx
@@ -4,6 +4,10 @@ const Me = () => (
   <div className="Me">
     <h1>Hi, I'm Anthony Greenheck</h1>
     <h2>Front-end Engineer</h2>
+    <div className="Links">
+      <a href="https://github.com/anthonyemg" target="_blank"><img src="../assets/github.svg" /></a>
+      <a href="https://www.linkedin.com/in/anthonygreenheck/" target="_blank"><img src="../assets/linkedin.svg" /></a>
+    </div>
   </div>
 );
 
