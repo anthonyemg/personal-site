890e25d0f9a63806f8e30625a2ccce22572537b7
Anthony Greenheck <aemgreenheck@gmail.com>
2017-07-27 14:00:46 -0400

    [added] worldFM pics and link:

--- src/public/app/Skills.jsx
+++ src/public/app/Skills.jsx
@@ -3,11 +3,9 @@ import React from 'react';
 const Skills = () => {
   return(
     <div className='Skills'>
-      <h1>React</h1>
-      <h1>React</h1>
-      <h1>React</h1>
-      <h1>React</h1>
-      <h1>React</h1>
+      <a href="http://worldfm.herokuapp.com/" target="_blank" className="WorldFMTitle">WorldFM</a>
+      <img src="../assets/worldFM1.png" className="WorldFM"/>
+      <img src="../assets/worldFM2.png" className="WorldFM"/>
     </div>
   )
 }
