f473eb21609bef211d49c32a9dde0be1d6791280
Anthony Greenheck <aemgreenheck@gmail.com>
2017-09-28 18:25:39 -0400

    [tweaked] responsiveness

--- src/public/app/components/Imstagran.js
+++ src/public/app/components/Imstagran.js
@@ -11,13 +11,16 @@ class Imstagran extends React.Component {
 
   render() {
     return(
-      <div className='imstagran' style={{width: this.props.width, height: this.props.height}}>
-        <a href='http://imstagran.io/' target='_blank' className='worldFM-title'>imstagran</a>
-        <span style={{fontSize: 20, width: this.props.width * 0.8, paddingBottom: 20,}}>
-          A React instagram clone simple made for fun.
+      <div
+        className='project'
+        style={{width: this.props.width, height: this.props.height}}
+      >
+        <a href='http://imstagran.io/' target='_blank' className='project-title'>imstagran</a>
+        <span style={{fontSize: 20, width: this.props.width * 0.7, paddingBottom: 20, textAlign: this.props.width < 560 ? 'left' : 'center'}}>
+          A React instagram clone simply made for fun.
         </span>
-        <div className='worldFM-images' style={{width: this.props.width * 0.8}}>
-          <img src='../assets/imstagranMock.png' style={{height: this.props.width < 500 ? 200 : 300}}/>
+        <div className='project-images' style={{width: this.props.width * 0.8}}>
+          <img src='../assets/imstagranMock.png' style={{height: this.props.width < 600 ? '46vw' : 280}}/>
         </div>
         <i className='icon fa fa-chevron-up faa-pulse animated' onClick={() => this.props.scrollToPage(this.state.previousPage)}/>
       </div>
