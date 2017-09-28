de39d48f67578948cffab0cbf27aef357418a6b9
Anthony Greenheck <aemgreenheck@gmail.com>
2017-09-28 17:42:06 -0400

    [updated] graphics

--- src/public/app/components/WorldFM.js
+++ src/public/app/components/WorldFM.js
@@ -18,11 +18,12 @@ class WorldFM extends React.Component {
         style={{width: this.props.width, height: this.props.height}}>
         <a href='http://worldfm.io/' target='_blank' className='worldFM-title'>WorldFM</a>
         <span style={{fontSize: 20, width: this.props.width * 0.8, paddingBottom: 20,}}>
-          WorldFM is a React/Redux Web App for discovering new music from around the world, powered by Spotify’s API
+          A React/Redux Web App for discovering new music from around the world, powered by Spotify’s API
         </span>
         <div className='worldFM-images' style={{width: this.props.width * 0.8}}>
-          <img src='https://i.imgur.com/lpSYKFJ.png' style={{height: this.props.width < 500 ? 200 : 300}}/>
-          <img src='https://image.ibb.co/iv6pdk/worldfm_2.png' style={{height: this.props.width < 500 ? 200 : 300}}/>
+          {/* <img src='https://i.imgur.com/lpSYKFJ.png' style={{height: this.props.width < 500 ? 200 : 300}}/>
+          <img src='https://image.ibb.co/iv6pdk/worldfm_2.png' style={{height: this.props.width < 500 ? 200 : 300}}/> */}
+          <img src='../assets/worldFmMock.png' style={{height: this.props.width < 500 ? 200 : 300}}/>
         </div>
         <div>
           <i className='icon fa fa-chevron-up faa-pulse animated' onClick={() => this.props.scrollToPage(this.state.previousPage)}/>
