2c7499bcd016b14af572cad7dfb97223559551db
Anthony Greenheck <aemgreenheck@gmail.com>
2017-09-28 16:03:03 -0400

    [updated] content

--- src/public/app/components/Imstagran.js
+++ src/public/app/components/Imstagran.js
@@ -13,6 +13,13 @@ class Imstagran extends React.Component {
     return(
       <div className='imstagran' style={{width: this.props.width, height: this.props.height}}>
         <a href='http://imstagran.io/' target='_blank' className='worldFM-title'>imstagran</a>
+        <span style={{fontSize: 20, width: this.props.width * 0.8, paddingBottom: 20,}}>
+          Imstagran is a React instagram clone simple made for fun.
+        </span>
+        <div className='worldFM-images' style={{width: this.props.width * 0.8}}>
+          <img src='https://i.imgur.com/lpSYKFJ.png' style={{height: this.props.width < 500 ? 200 : 300}}/>
+          <img src='https://image.ibb.co/iv6pdk/worldfm_2.png' style={{height: this.props.width < 500 ? 200 : 300}}/>
+        </div>
         <i className='icon fa fa-chevron-up faa-pulse animated' onClick={() => this.props.scrollToPage(this.state.previousPage)}/>
       </div>
     )
--- src/public/app/components/WorldFM.js
+++ src/public/app/components/WorldFM.js
@@ -17,12 +17,13 @@ class WorldFM extends React.Component {
         className='worldFM-container'
         style={{width: this.props.width, height: this.props.height}}>
         <a href='http://worldfm.io/' target='_blank' className='worldFM-title'>WorldFM</a>
-        <span style={{fontSize: 20, width: this.props.width * 0.8, paddingBottom: 20,}}>WorldFM is a React/Redux Web App for discovering new music from around the world, powered by Spotify’s API</span>
+        <span style={{fontSize: 20, width: this.props.width * 0.8, paddingBottom: 20,}}>
+          WorldFM is a React/Redux Web App for discovering new music from around the world, powered by Spotify’s API
+        </span>
         <div className='worldFM-images' style={{width: this.props.width * 0.8}}>
           <img src='https://i.imgur.com/lpSYKFJ.png' style={{height: this.props.width < 500 ? 200 : 300}}/>
           <img src='https://image.ibb.co/iv6pdk/worldfm_2.png' style={{height: this.props.width < 500 ? 200 : 300}}/>
         </div>
-        <span className="worldFM-moreToCome">more to come..</span>
         <div>
           <i className='icon fa fa-chevron-up faa-pulse animated' onClick={() => this.props.scrollToPage(this.state.previousPage)}/>
           <i className='icon fa fa-chevron-down faa-pulse animated' onClick={() => this.props.scrollToPage(this.state.nextPage)}/>
